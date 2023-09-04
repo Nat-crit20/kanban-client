f07c8a7865de86ebb226c1296a6e9b830b3c1f1c
Nat-crit20 <greuelnathan74@gmail.com>
2023-09-04 13:28:59 -0500

    Created sign up and login view

--- src/components/Welcome/WelcomeView.js
+++ src/components/Welcome/WelcomeView.js
@@ -1,33 +1,12 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import { LoginView } from "../Login/LoginView";
+import { SignUpView } from "../SignUp/SignUpView";
 
 export const WelcomeView = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <div>
+      <h1>Welcome</h1>
+      <SignUpView />
+      <LoginView />
+    </div>
   );
 };
