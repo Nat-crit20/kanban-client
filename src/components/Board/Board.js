602baf583e9594db9456174891e0e319e9a7061b
Nat-crit20 <greuelnathan74@gmail.com>
2023-10-11 09:04:20 -0500

    moved the board header to outside the board view

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,24 +3,16 @@ import { ColumnsView } from "../Columns/Columns";
 import Modal from "react-bootstrap/Modal";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { ReactComponent as VerticalEllipse } from "../../assets/icon-vertical-ellipsis.svg";
+
 import "./Board.css";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 export const BoardView = ({ currentBoard, token, updateCurrentBoard }) => {
   const [showColumn, setShowColumn] = useState(false);
   const handleColumnClose = () => setShowColumn(false);
   const handleColumnShow = () => setShowColumn(true);
 
-  const [showTask, setShowTask] = useState(false);
-  const handleTaskClose = () => setShowTask(false);
-  const handleTaskShow = () => setShowTask(true);
-
   const [columnName, setColumnName] = useState(null);
-  const [taskName, setTaskName] = useState(null);
-  const [taskDescription, setTaskDescription] = useState(null);
-  const [subtasks, setSubtasks] = useState([]);
-  const taskStatus = useRef();
 
   const handleColumnSubmit = (e) => {
     e.preventDefault();
@@ -54,77 +46,10 @@ export const BoardView = ({ currentBoard, token, updateCurrentBoard }) => {
       });
   };
 
-  const handleTaskSubmit = (e) => {
-    e.preventDefault();
-    const selectedOption = taskStatus.current;
-    const selectedId = selectedOption.value;
-    const columnName = currentBoard.Columns.filter((column) => {
-      return column._id === selectedId;
-    });
-    const data = {
-      Title: taskName,
-      Description: taskDescription,
-      Status: { name: columnName[0].Name, columnID: columnName[0]._id },
-      SubTasks: subtasks,
-    };
-
-    fetch(
-      `https://obscure-river-59850-ea6dbafa2f33.herokuapp.com/column/${columnName[0]._id}/task`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify(data),
-      }
-    )
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then((data) => {
-        const columnIndex = currentBoard.Columns.findIndex(
-          (column) => column._id === data._id
-        );
-
-        if (columnIndex !== -1) {
-          // Create a shallow copy of boardState
-          const updatedBoard = { ...currentBoard };
-          // Replace the old column with the updated data
-          updatedBoard.Columns[columnIndex] = data;
-          // Update the board state with the new data
-          updateCurrentBoard(updatedBoard);
-        }
-
-        handleTaskClose();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   return (
     <>
       {currentBoard ? (
         <>
-          <div className="board-header">
-            <h1>{currentBoard.Name}</h1>
-            {currentBoard &&
-            currentBoard.Columns &&
-            currentBoard.Columns.length > 0 ? (
-              <div>
-                <Button variant="primary" onClick={handleTaskShow}>
-                  Add New Task
-                </Button>
-                <VerticalEllipse />
-              </div>
-            ) : (
-              <></>
-            )}
-          </div>
           <div>
             <Row className="column-home">
               {currentBoard &&
@@ -177,105 +102,6 @@ export const BoardView = ({ currentBoard, token, updateCurrentBoard }) => {
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="secondary" onClick={handleColumnClose}>
-                  Close
-                </Button>
-              </Modal.Footer>
-            </Modal>
-
-            <Modal show={showTask} onHide={handleTaskClose} centered>
-              <Modal.Header closeButton>
-                <Modal.Title>Add New Task</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <Modal.Body>
-                  <form
-                    method="post"
-                    className="form-task"
-                    onSubmit={handleTaskSubmit}
-                  >
-                    <div className="form-signup">
-                      <label htmlFor="name">Title: </label>
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        value={taskName}
-                        onChange={(e) => setTaskName(e.target.value)}
-                        required
-                      />
-                      <label htmlFor="description">Description: </label>
-                      <input
-                        type="text"
-                        name="description"
-                        id="description"
-                        value={taskDescription}
-                        onChange={(e) => setTaskDescription(e.target.value)}
-                        required
-                      />
-                      <label htmlFor="subtask">Status: </label>
-
-                      {currentBoard && currentBoard.Columns ? (
-                        <select id="status" name="status" ref={taskStatus}>
-                          {currentBoard.Columns.map((column) => (
-                            <option
-                              key={column._id}
-                              value={column._id}
-                              data-name={column.Name}
-                            >
-                              {column.Name}
-                            </option>
-                          ))}
-                        </select>
-                      ) : (
-                        <p>Loading or no data available.</p>
-                      )}
-                      <label htmlFor="subtask">Subtask: </label>
-                      {subtasks.map((subtask, i) => (
-                        <div key={i}>
-                          <input
-                            type="text"
-                            name="subtask"
-                            id={`subtask_${i}`} // Add a unique id for each input
-                            value={subtask.title}
-                            onChange={(e) => {
-                              const updatedSubtasks = [...subtasks]; // Create a copy of the subtasks array
-                              updatedSubtasks[i] = { title: e.target.value }; // Update the specific subtask
-                              setSubtasks(updatedSubtasks); // Update the state with the new array
-                            }}
-                          />
-                          <span
-                            onClick={() => {
-                              const updatedSubtasks = subtasks.filter(
-                                (subtask, j) => {
-                                  // Return true to keep the subtask, false to remove it
-                                  return i !== j; // Replace indexToRemove with the index you want to remove
-                                }
-                              );
-                              setSubtasks(updatedSubtasks);
-                            }}
-                          >
-                            x
-                          </span>
-                        </div>
-                      ))}
-                      <Button
-                        onClick={() => {
-                          const currentSubtasks = [...subtasks, {}];
-                          setSubtasks(currentSubtasks);
-                        }}
-                      >
-                        + Add New Subtask
-                      </Button>
-                    </div>
-
-                    <div className="form-signup">
-                      <input type="submit" value="Submit" />
-                    </div>
-                  </form>
-                </Modal.Body>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleTaskClose}>
                   Close
                 </Button>
               </Modal.Footer>
--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -11,11 +11,12 @@ import { ReactComponent as LogoForLight } from "../../assets/logo-dark.svg";
 import { ReactComponent as LogoForDark } from "../../assets/logo-light.svg";
 import { ReactComponent as HideSidebarSvg } from "../../assets/icon-hide-sidebar.svg";
 import { ReactComponent as ShowSidebarSvg } from "../../assets/icon-show-sidebar.svg";
+import { ReactComponent as VerticalEllipse } from "../../assets/icon-vertical-ellipsis.svg";
 import { ReactComponent as DarkModeSvg } from "../../assets/icon-dark-theme.svg";
 import { ReactComponent as LightModeSvg } from "../../assets/icon-light-theme.svg";
 
 import "./HomeView.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export const HomeView = ({
   user,
@@ -27,8 +28,16 @@ export const HomeView = ({
 }) => {
   const [show, setShow] = useState(false);
   const [boardName, setBoardName] = useState(null);
+  const [taskName, setTaskName] = useState(null);
+  const [taskDescription, setTaskDescription] = useState(null);
+  const [subtasks, setSubtasks] = useState([]);
+  const taskStatus = useRef();
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [showTask, setShowTask] = useState(false);
+  const handleTaskClose = () => setShowTask(false);
+  const handleTaskShow = () => setShowTask(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -89,12 +98,81 @@ export const HomeView = ({
       });
   };
 
+  const handleTaskSubmit = (e) => {
+    e.preventDefault();
+    const selectedOption = taskStatus.current;
+    const selectedId = selectedOption.value;
+    const columnName = currentBoard.Columns.filter((column) => {
+      return column._id === selectedId;
+    });
+    const data = {
+      Title: taskName,
+      Description: taskDescription,
+      Status: { name: columnName[0].Name, columnID: columnName[0]._id },
+      SubTasks: subtasks,
+    };
+
+    fetch(
+      `https://obscure-river-59850-ea6dbafa2f33.herokuapp.com/column/${columnName[0]._id}/task`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        const columnIndex = currentBoard.Columns.findIndex(
+          (column) => column._id === data._id
+        );
+
+        if (columnIndex !== -1) {
+          // Create a shallow copy of boardState
+          const updatedBoard = { ...currentBoard };
+          // Replace the old column with the updated data
+          updatedBoard.Columns[columnIndex] = data;
+          // Update the board state with the new data
+          updateCurrentBoard(updatedBoard);
+        }
+
+        handleTaskClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <Container className="home-screen m-0 p-0">
         <Row>
-          <Col xs md={3} className="sidebar" style={{ display: "flex" }}>
+          <div className="board-header">
             <LogoForLight className="sidebar-head" />
+            <h1>{currentBoard ? currentBoard.Name : ""}</h1>
+            {currentBoard &&
+            currentBoard.Columns &&
+            currentBoard.Columns.length > 0 ? (
+              <div>
+                <Button variant="primary" onClick={handleTaskShow}>
+                  Add New Task
+                </Button>
+                <VerticalEllipse />
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+        </Row>
+        <Row>
+          <Col xs md={3} className="sidebar" style={{ display: "flex" }}>
             <ButtonGroup vertical>
               <p className="sidebar-head">ALL BOARDS ({boards.length})</p>
               {boards.map((board) => {
@@ -182,6 +260,104 @@ export const HomeView = ({
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal show={showTask} onHide={handleTaskClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Add New Task</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Modal.Body>
+              <form
+                method="post"
+                className="form-task"
+                onSubmit={handleTaskSubmit}
+              >
+                <div className="form-signup">
+                  <label htmlFor="name">Title: </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={taskName}
+                    onChange={(e) => setTaskName(e.target.value)}
+                    required
+                  />
+                  <label htmlFor="description">Description: </label>
+                  <input
+                    type="text"
+                    name="description"
+                    id="description"
+                    value={taskDescription}
+                    onChange={(e) => setTaskDescription(e.target.value)}
+                    required
+                  />
+                  <label htmlFor="subtask">Status: </label>
+
+                  {currentBoard && currentBoard.Columns ? (
+                    <select id="status" name="status" ref={taskStatus}>
+                      {currentBoard.Columns.map((column) => (
+                        <option
+                          key={column._id}
+                          value={column._id}
+                          data-name={column.Name}
+                        >
+                          {column.Name}
+                        </option>
+                      ))}
+                    </select>
+                  ) : (
+                    <p>Loading or no data available.</p>
+                  )}
+                  <label htmlFor="subtask">Subtask: </label>
+                  {subtasks.map((subtask, i) => (
+                    <div key={i}>
+                      <input
+                        type="text"
+                        name="subtask"
+                        id={`subtask_${i}`} // Add a unique id for each input
+                        value={subtask.title}
+                        onChange={(e) => {
+                          const updatedSubtasks = [...subtasks]; // Create a copy of the subtasks array
+                          updatedSubtasks[i] = { title: e.target.value }; // Update the specific subtask
+                          setSubtasks(updatedSubtasks); // Update the state with the new array
+                        }}
+                      />
+                      <span
+                        onClick={() => {
+                          const updatedSubtasks = subtasks.filter(
+                            (subtask, j) => {
+                              // Return true to keep the subtask, false to remove it
+                              return i !== j; // Replace indexToRemove with the index you want to remove
+                            }
+                          );
+                          setSubtasks(updatedSubtasks);
+                        }}
+                      >
+                        x
+                      </span>
+                    </div>
+                  ))}
+                  <Button
+                    onClick={() => {
+                      const currentSubtasks = [...subtasks, {}];
+                      setSubtasks(currentSubtasks);
+                    }}
+                  >
+                    + Add New Subtask
+                  </Button>
+                </div>
+
+                <div className="form-signup">
+                  <input type="submit" value="Submit" />
+                </div>
+              </form>
+            </Modal.Body>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleTaskClose}>
               Close
             </Button>
           </Modal.Footer>
