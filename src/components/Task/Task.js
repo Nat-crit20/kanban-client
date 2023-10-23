96b3403b8d3c33b9f3ac34f4c224215a2a057814
Nat-crit20 <greuelnathan74@gmail.com>
2023-10-23 09:25:28 -0500

    Styling the task info and added modal

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { Button } from "react-bootstrap";
 export const TaskView = ({ task }) => {
+  const [showTask, setShowColumn] = useState(false);
+  const handleTaskClose = () => setShowColumn(false);
+  const handleTaskShow = () => setShowColumn(true);
   return (
     <>
-      <h3>{task.Title}</h3>
+      <div className="task-info" onClick={handleTaskShow}>
+        <h3>{task.Title}</h3>
+      </div>
+      <Modal show={showTask} onHide={handleTaskClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Modal.Body></Modal.Body>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleTaskClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
