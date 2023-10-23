fbfd6d69ace402775754b6ab028428d8789f732c
Nat-crit20 <greuelnathan74@gmail.com>
2023-10-23 09:38:55 -0500

    Styled the task info

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import { Button } from "react-bootstrap";
+import "./Task.css";
 export const TaskView = ({ task }) => {
   const [showTask, setShowColumn] = useState(false);
   const handleTaskClose = () => setShowColumn(false);
   const handleTaskShow = () => setShowColumn(true);
   return (
     <>
-      <div className="task-info" onClick={handleTaskShow}>
-        <h3>{task.Title}</h3>
+      <div id="task-info" onClick={handleTaskShow}>
+        <div className="task-info-text">
+          <h3>{task.Title}</h3>
+          <p>Number of subtask completed</p>
+        </div>
       </div>
       <Modal show={showTask} onHide={handleTaskClose} centered>
         <Modal.Header closeButton>
