6b22263dff1bf08a10c52937484b1b372f3b9f32
Nat-crit20 <greuelnathan74@gmail.com>
2023-11-23 11:05:02 -0600

    Updated column width

--- src/components/Columns/Columns.js
+++ src/components/Columns/Columns.js
@@ -1,8 +1,9 @@
 import { TaskView } from "../Task/Task";
+import "./Column.css";
 export const ColumnsView = ({ column, boardColumns }) => {
   return (
     <>
-      <h1>{column.Name}</h1>
+      <h1 className="column-name">{column.Name}</h1>
       {column.Tasks.map((task) => {
         return <TaskView task={task} boardColumns={boardColumns} />;
       })}
