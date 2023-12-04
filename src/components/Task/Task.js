64fa7fb953c825f253c825ea2b7ee0edf0b0be19
Nat-crit20 <greuelnathan74@gmail.com>
2023-12-03 19:19:20 -0600

    Added Edit task modal

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,21 +1,33 @@
 import { useState } from "react";
 import { ReactComponent as VerticalEllipse } from "../../assets/icon-vertical-ellipsis.svg";
+import { ReactComponent as IconCross } from "../../assets/icon-cross.svg";
+
 import Modal from "react-bootstrap/Modal";
-import { Button } from "react-bootstrap";
 import "./Task.css";
 export const TaskView = ({ currentColumn, task, boardColumns, token }) => {
-  const [showTask, setShowColumn] = useState(false);
+  const [showTask, setShowTask] = useState(false);
+  const [showEditTask, setEditShowTask] = useState(false);
   const [currentTask, setCurrentTask] = useState(task);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [taskName, setTaskName] = useState(null);
+  const [taskDescription, setTaskDescription] = useState(null);
+  const [subtasks, setSubtasks] = useState([]);
 
   const handleDropdownTask = () => {
     setDropdownOpen(!dropdownOpen);
   };
   const handleTaskClose = () => {
     handleTaskUpdate();
-    setShowColumn(false);
+    setShowTask(false);
   };
-  const handleTaskShow = () => setShowColumn(true);
+  const handleEditTaskClose = () => {
+    setEditShowTask(false);
+  };
+  const handleTaskShow = () => setShowTask(true);
+  const handleEditTaskShow = () => {
+    handleTaskClose();
+    setEditShowTask(true);
+  };
 
   const handleCheckboxChange = (subtaskId) => {
     setCurrentTask((prevInfo) => ({
@@ -99,7 +111,9 @@ export const TaskView = ({ currentColumn, task, boardColumns, token }) => {
                     display: dropdownOpen ? "block" : "none",
                   }}
                 >
-                  <p className="edit-board ">Edit Task</p>
+                  <p className="edit-board " onClick={handleEditTaskShow}>
+                    Edit Task
+                  </p>
                   <p className="delete-board ">Delete Task</p>
                 </div>
               </div>
@@ -173,6 +187,124 @@ export const TaskView = ({ currentColumn, task, boardColumns, token }) => {
               </select>
             </form>
           </div>
+        </Modal.Body>
+      </Modal>
+      <Modal
+        id="create-task-modal"
+        show={showEditTask}
+        onHide={handleEditTaskClose}
+        centered
+      >
+        <Modal.Body>
+          <Modal.Body>
+            <h3>Add New Task</h3>
+            <form method="post" className="form-task">
+              <div className="form-create-task">
+                <label htmlFor="name">Title: </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={taskName}
+                  onChange={(e) => setTaskName(e.target.value)}
+                  required
+                />
+                <label htmlFor="description">Description: </label>
+                <textarea
+                  type="text"
+                  name="description"
+                  rows="4"
+                  cols="5"
+                  id="description"
+                  value={taskDescription}
+                  onChange={(e) => setTaskDescription(e.target.value)}
+                />
+
+                <label htmlFor="subtask">Subtask: </label>
+                {subtasks.map((subtask, i) => (
+                  <div key={i}>
+                    <input
+                      type="text"
+                      className="create-subtask"
+                      name="subtask"
+                      id={`subtask_${i}`} // Add a unique id for each input
+                      value={subtask.title}
+                      onChange={(e) => {
+                        const updatedSubtasks = [...subtasks]; // Create a copy of the subtasks array
+                        updatedSubtasks[i] = { title: e.target.value }; // Update the specific subtask
+                        setSubtasks(updatedSubtasks); // Update the state with the new array
+                      }}
+                      required
+                    />
+                    <span
+                      onClick={() => {
+                        const updatedSubtasks = subtasks.filter(
+                          (subtask, j) => {
+                            // Return true to keep the subtask, false to remove it
+                            return i !== j; // Replace indexToRemove with the index you want to remove
+                          }
+                        );
+                        setSubtasks(updatedSubtasks);
+                      }}
+                    >
+                      <IconCross />
+                    </span>
+                  </div>
+                ))}
+                <button
+                  className="add-subtask"
+                  onClick={() => {
+                    const currentSubtasks = [...subtasks, {}];
+                    setSubtasks(currentSubtasks);
+                  }}
+                >
+                  + Add New Subtask
+                </button>
+                <label htmlFor="status">Status: </label>
+
+                {boardColumns ? (
+                  <select
+                    id="status"
+                    name="status"
+                    onChange={handleColumnChange}
+                  >
+                    {boardColumns.map((column) => {
+                      if (currentTask.Status.columnID === column._id) {
+                        return (
+                          <option
+                            key={column._id}
+                            value={column._id}
+                            data-name={column.Name}
+                            selected
+                          >
+                            {column.Name}
+                          </option>
+                        );
+                      } else {
+                        return (
+                          <option
+                            key={column._id}
+                            value={column._id}
+                            data-name={column.Name}
+                          >
+                            {column.Name}
+                          </option>
+                        );
+                      }
+                    })}
+                  </select>
+                ) : (
+                  <p>Loading or no data available.</p>
+                )}
+              </div>
+
+              <input
+                className="form-submit"
+                type="submit"
+                value="Create New Task"
+              />
+            </form>
+          </Modal.Body>
         </Modal.Body>
       </Modal>
     </>
