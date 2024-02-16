7e57cce813eff374c761c9de9940759b6554febd
Nat-crit20 <greuelnathan74@gmail.com>
2024-02-16 12:22:40 -0600

    Added navbar to the welcome page

--- src/components/Welcome/WelcomeView.js
+++ src/components/Welcome/WelcomeView.js
@@ -1,9 +1,28 @@
 import { LoginView } from "../Login/LoginView";
 import { SignUpView } from "../SignUp/SignUpView";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { ReactComponent as LogoForLight } from "../../assets/logo-dark.svg";
 import "./WelcomeView.scss";
 export const WelcomeView = ({ onLogin }) => {
   return (
     <div className="welcome-view">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <LogoForLight />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Contact</Nav.Link>
+              <Nav.Link href="#link">Get started</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <h1>Welcome to Kanban</h1>
       <div className="welcome-actions">
         <SignUpView />
